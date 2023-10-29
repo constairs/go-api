24986f5378c7ad9f1a79a712bf46b55b7bb8d86f
constairs <constairs@gmail.com>
2023-10-30 00:21:54 +0700

    Initial commit

---
+++ internal/http-server/handlers/url/remove/remove.go
@@ -0,0 +1,58 @@
+package remove
+
+import (
+	"errors"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"log/slog"
+	"net/http"
+	resp "url-shortener/internal/lib/api/response"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+)
+
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLRemover
+type URLRemover interface {
+	DeleteURL(alias string) error
+}
+
+func New(log *slog.Logger, urlRemover URLRemover) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.remove.New"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		alias := chi.URLParam(r, "alias")
+		if alias == "" {
+			log.Info("alias is empty")
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+
+		err := urlRemover.DeleteURL(alias)
+		if errors.Is(err, storage.ErrURLNotFound) {
+			log.Info("url not found", "alias", alias)
+
+			render.JSON(w, r, resp.Error("not found"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to remove url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("internal error"))
+
+			return
+		}
+
+		log.Info("remove url", slog.String("url for alias", alias))
+
+		resp.OK()
+	}
+}
